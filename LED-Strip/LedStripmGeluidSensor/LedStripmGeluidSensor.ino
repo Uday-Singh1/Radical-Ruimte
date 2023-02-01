78f7de8bee26ea4ff3db799f56308b570ac49b61
AhmetAMA <33016@ma-web.nl>
2023-02-01 23:03:52 +0100

    uitleg toegevoegd aan de code

--- LED-Strip/LedStripmGeluidSensor/LedStripmGeluidSensor.ino
+++ LED-Strip/LedStripmGeluidSensor/LedStripmGeluidSensor.ino
@@ -1,16 +1,17 @@
-int soundsensor = 2;
-int LED = 3;
+int soundsensor = 2;  //  Here I define the SoundSensor to pin 2 in the Arduino Uno.
+int LED = 3;          //  Here I define the LED-Strip to pin 3 in the Arduino Uno.
 
-void setup(){
-  pinMode (soundsensor, INPUT);
-  pinMode (LED, OUTPUT);
+void setup(){ //  Here I create a setup function.
+  pinMode (soundsensor, INPUT); //  I set the value to input because it records the sound.
+  pinMode (LED, OUTPUT);        //  I set the value to output because it displays the light.
 }
 
-void loop(){
-  int statusSensor = digitalRead(soundsensor);
-  if (statusSensor ==1) { digitalWrite(LED ,HIGH);
+void loop(){  //  Here I create a loop function.
+  int statusSensor = digitalRead(soundsensor);  //  The digitalRead reads the current situation at that moment.
+  if (statusSensor ==1) { digitalWrite(LED ,HIGH);  //  Checks if the value of 'statusSensor' is equal to 1 and sets the 'LED' pin
+                                                    //  to high state (5V) to turn on the connected component if it is.
 }
 
-else{
-  digitalWrite(LED ,LOW);
+else{ //  If the loop function is not used, this function will go in.
+  digitalWrite(LED ,LOW); //  Sets the specified digital pin 'LED' to a low state (0V) which turns off the connected component.
 }
